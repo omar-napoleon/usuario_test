cda67e9f7e756aef592047bbc61595ae2b6fb953
usuario <usuario@MacBook-Pro-de-usuario.local>
2019-11-18 13:40:07 -0300

    se agrega gradle

--- src/main/java/com/example/PersonApplication.java
+++ src/main/java/com/example/PersonApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PersonaApplication {
+public class PersonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PersonaApplication.class, args);
+		SpringApplication.run(PersonApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/PersonApplicationTest.java
+++ src/test/java/com/example/PersonApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PersonaApplicationTests {
+public class PersonApplicationTest {
 
 	@Test
 	public void contextLoads() {
